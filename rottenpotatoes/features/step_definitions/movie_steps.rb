5f909883b2980174d19ef8a9424d33c4968758c2
aonomike <aonomike@gmail.com>
2018-10-03 18:11:16 +0300

    Add cucumber test scenarios and features

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -29,7 +29,23 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  if uncheck
+    # uncheck all the boxes whose values are in rating_list
+    ratings_list.split.each |rating| do
+
+    end
+  else
+    #check all the boxes in rating_list
+  end
+
+  # steps( %Q(
+  #   Then I visit "/movies"
+  #     And I enter "#{user}" in the "user name" field
+  #     And I enter "#{user}-test-passwd" in the "password" field
+  #     And I press the "login" button
+  # ) )
+
+
 end
 
 Then /I should see all the movies/ do
